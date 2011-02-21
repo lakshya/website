4a35bf6918f377b1201377b9a9989b9ea5b9f933
unknown <Harish@.(none)>
2011-02-22 00:26:47 +0530

    test comment

--- www/application/views/home/aboutUs.php
+++ www/application/views/home/aboutUs.php
@@ -1,7 +1,7 @@
 <!--<p> Motto:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Transforming lives through education.</p>-->
 <p> The Lakshya Foundation is a registerd trust founded by the alumni of NIT Warangal on 7th June, 2008.</p>
 <!--<p> Founded by: &nbsp; &nbsp; &nbsp;Srihari Maneru, Anand Rajagopalan.</p>-->
-<p> Its chief mentors are Prof. J.Ramachandra Rao and Dr. K.Padma, both faculty members at NIT Warangal.</p>
+<p> Its Chief mentors are Prof. J.Ramachandra Rao and Dr. K.Padma, both faculty members at NIT Warangal.</p>
 <p> Coming face to face with a real problem where some students were in need of financial help to continue their education, we realized that money should not be a hurdle for education. Funds were raised and the tution fees of the students were paid. Thus, the Lakshya Foundation was born.&nbsp;</p>
 <p> The Lakshya Foundation initiated &lsquo;Project 1&rsquo; which would later be known  as &ldquo;Lakshya scholarships&rdquo;. As part of this project, intitute fees (tuition fee + hostel fee) are paid for students coming from poor financial backgrounds. It is ensured that the verification is fool proof by personally visiting the applicant&rsquo;s residence. In 2010, Lakshya moved from a grants based model to a loan based model to endure sustainability and to reach out to many more students.</p>
 <p> Over the years, Lakshya has moved on from just providing scholarships. Given below is a list of all the projects we have or are working on:</p>

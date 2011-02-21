27b437df1dbd6705281fc75c9737a232ae76a04a
Anand Rajagopalan <anandr31@anandr31-laptop.(none)>
2011-02-22 00:48:00 +0530

    Content change Removed Chief mentors line.

--- www/application/views/home/aboutUs.php
+++ www/application/views/home/aboutUs.php
@@ -1,13 +1,10 @@
-<!--<p> Motto:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Transforming lives through education.</p>-->
 <p> The Lakshya Foundation is a registerd trust founded by the alumni of NIT Warangal on 7th June, 2008.</p>
-<!--<p> Founded by: &nbsp; &nbsp; &nbsp;Srihari Maneru, Anand Rajagopalan.</p>-->
-<p> Its Chief mentors are Prof. J.Ramachandra Rao and Dr. K.Padma, both faculty members at NIT Warangal.</p>
 <p> Coming face to face with a real problem where some students were in need of financial help to continue their education, we realized that money should not be a hurdle for education. Funds were raised and the tution fees of the students were paid. Thus, the Lakshya Foundation was born.&nbsp;</p>
 <p> The Lakshya Foundation initiated &lsquo;Project 1&rsquo; which would later be known  as &ldquo;Lakshya scholarships&rdquo;. As part of this project, intitute fees (tuition fee + hostel fee) are paid for students coming from poor financial backgrounds. It is ensured that the verification is fool proof by personally visiting the applicant&rsquo;s residence. In 2010, Lakshya moved from a grants based model to a loan based model to endure sustainability and to reach out to many more students.</p>
 <p> Over the years, Lakshya has moved on from just providing scholarships. Given below is a list of all the projects we have or are working on:</p>
 <p> 1)&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="<?=url::base().'scholarships'?>">Lakshya Scholarships</a></strong>&nbsp;&ndash; Provide funding for students who are unable to pay for their education.</p>
-<p> 2)&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="<?=url::base().'asthra'?>">ASTHRA</a></strong>&nbsp;&ndash; Ensure transparency in NITW with RTI as a tool.</p>
-<p> 3)&nbsp;&nbsp;&nbsp;<strong>&nbsp;<a href="<?=url::base().'innovation'?>">The Innovation Project</a></strong>&nbsp;&ndash; Converting bright ideas into realities, without worrying about the financial foe.</p>
+<p> 2)&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="<?=url::base().'asthra'?>">ASTHRA</a></strong>&nbsp;&ndash; Ensure transparency using the Right to Information Act.</p>
+<p> 3)&nbsp;&nbsp;&nbsp;<strong>&nbsp;<a href="<?=url::base().'innovation'?>">The Innovation Project</a></strong>&nbsp;&ndash; Converting bright ideas into realities, without worrying about finances.</p>
 <p> 4)&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="<?=url::base().'library'?>">The Library Project</a></strong>&nbsp;&ndash; Providing non curriculum books to students.</p>
 <p> 5)&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="<?=url::base().'ekjodikapda'?>">EK JODI KAPDA</a></strong>&nbsp;&ndash; &lsquo;A SET OF CLOTHES&rsquo; for the 456 million Indians living below poverty line.</p>
 <!--<p> With Transparency as the Mantra, and Hard work as the tool, Lakshya strives to &ldquo;Transform lives through Education&rdquo;.</p>-->

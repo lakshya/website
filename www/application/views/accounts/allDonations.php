2fc5cef99f16838887dc02cda0f1153bd2ac6e88
Anand Rajagopalan <anandr31@anandr31-laptop.(none)>
2011-02-28 21:17:37 +0530

    [code] corrections in sorting css and html code

--- www/application/views/accounts/allDonations.php
+++ www/application/views/accounts/allDonations.php
@@ -9,25 +9,25 @@ Updated on: February 15, 2011
 <center>
 <!-- old header row colour: bgcolor="#D7D6E9" -->
 <!-- old body colour: bgcolor="#efefef" -->
-<table id="test1" class="sortable-onload-5-reverse rowstyle-alt no-arrow" width="100%" border="0" cellspacing="1">
+<table id="donTable" class="sort sortable-onload-5-reverse rowstyle-alt no-arrow" width="100%" border="0" cellspacing="1">
 <thead>
-	<tr>
-		<th style="-moz-user-select: none;" class="fd-column-0 sortable-text"><a title="Sort on “Name”" href="#"><b>Name</b></th>
-		<th style="-moz-user-select: none;" class="fd-column-1 sortable-numeric"><a title="Sort on “Batch”" href="#"><b>Batch</b></th>
-		<th style="-moz-user-select: none;" class="fd-column-2 sortable-text"><a title="Sort on “Branch”" href="#"><b>Branch</b></th>
-		<th style="-moz-user-select: none;" class="fd-column-3 sortable-numeric "><a title="Sort on “Amount”" href="#"><b>Amount</b></th>
-		<th style="-moz-user-select: none;" class="fd-column-4 sortable-date-dmy"><a title="Sort on “Last Donated on”" href="#"><b>Last Donated on</b></th>
+	<tr class="sort">
+		<th class="sort fd-column-0 sortable-text"><a title="Sort on “Name”" href="#"><b>Name</b></th>
+		<th class="sort fd-column-1 sortable-numeric"><a title="Sort on “Batch”" href="#"><b>Batch</b></th>
+		<th class="sort fd-column-2 sortable-text"><a title="Sort on “Branch”" href="#"><b>Branch</b></th>
+		<th class="sort fd-column-3 sortable-numeric "><a title="Sort on “Amount”" href="#"><b>Amount</b></th>
+		<th class="sort fd-column-4 sortable-date-dmy"><a title="Sort on “Last Donated on”" href="#"><b>Last Donated on</b></th>
 	</tr>
 </thead>
 <tbody>
 
 <?foreach($data as $row):?>
-	<tr class="">
-		<td><?=$row->name?></td>
-		<td><?=$row->batch?></td>
-		<td><?=$row->branch?></td>
-		<td><?=$row->donAmount?></td>
-		<td><?=date('d/m/Y',strtotime($row->lastDon))?></td>
+	<tr class="sort">
+		<td class="sort"><?=$row->name?></td>
+		<td class="sort"><?=$row->batch?></td>
+		<td class="sort"><?=$row->branch?></td>
+		<td class="sort"><?=$row->donAmount?></td>
+		<td class="sort"><?=date('d/m/Y',strtotime($row->lastDon))?></td>
 	</tr>
 <?endforeach?>
 

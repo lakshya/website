944c84bac7dd41067200c273012b0e53567f0345
Anand Rajagopalan <anandr31@anandr31-laptop.(none)>
2011-03-04 01:31:59 +0530

    [code] live streaming page

---
+++ www/application/views/home/lakshyaLive.php
@@ -0,0 +1,14 @@
+
+<p align="center">
+Please refresh the page if the video does not stream even after the scheduled start time. Alernatively, you can click on the refresh icon on the player itself, but this sometimes does not work as expected.
+</p>
+<p>
+<b>Event:</b> The Innovation Project - Presentations by shortlisted teams (Final round)<br/>
+<b>Date:</b> Saturday, 5th March 2011<br/>
+<b>Time:</b> 2:30 p.m. onwards<br/>
+<b>Venue:</b> EEE Seminar Hall, NIT Warangal<br/>
+</p>
+<p align="center">
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="480" height="296" id="utv412420"><param name="flashvars" value="autoplay=false&amp;brand=embed&amp;cid=7364367&amp;v3=1"/><param name="allowfullscreen" value="true"/><param name="allowscriptaccess" value="always"/><param name="movie" value="http://www.ustream.tv/flash/viewer.swf"/><embed flashvars="autoplay=false&amp;brand=embed&amp;cid=7364367&amp;v3=1" width="480" height="296" allowfullscreen="true" allowscriptaccess="always" id="utv412420" name="utv_n_135963" src="http://www.ustream.tv/flash/viewer.swf" type="application/x-shockwave-flash" /></object><br /><a href="http://www.ustream.tv/" style="padding: 2px 0px 4px; width: 400px; background: #ffffff; display: block; color: #000000; font-weight: normal; font-size: 10px; text-decoration: underline; text-align: center;" target="_blank">Streaming Video by Ustream.TV</a>
+</p>
+

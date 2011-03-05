1cee17330914b0f63c2705a27eea0405600de9a5
harishsvs <harishsvs@gmail.com>
2011-03-05 07:11:52 -0800

    content changes only. review plz.

--- www/application/views/news/scroller.php
+++ www/application/views/news/scroller.php
@@ -1,28 +1,36 @@
 <?
 function slashit($what){ //Encode text for storing in JavaScript array
-$newstring=str_replace('&apos;', '\'', $what); //replace those half valid apostrophe entities with actual apostrophes
+$newstring=str_replace('&apos;', '\'', $what); //replace those half valid apostrophe 
+
+entities with actual apostrophes
 return rawurlencode($newstring);
 }
 ?>
 rsscontentdata.news=new Array();
 
 rsscontentdata.news[0] = {
-		link:"<?=url::base().'news/scholForm'?>",
-		title:"The application for the Lakshya Scholarship 2010 - 2011 is now closed . <br/>Click here for more details.",
+		link:"<?=url::base().'library'?>",
+		title:"<   Library project   > <br/><br/>Encouraging students to 
+
+embrace the book reading habit. <br/> Donate a book today!",
 		description:" ",
 		date:""
 	};
 
 rsscontentdata.news[1] = {
-		link:"<?=url::base().'gallery'?>",
-		title:"'Ek Jodi Kapda' project pics are uploaded on the website . <br/>Click here to view them.",
+		link:"<?=url::base().'innovation'?>",
+		title:"<   Innovation project   > <br/><br/>Live coverage of the 
+
+presentations streamed at Lakshya Live!! <br/>Click here to view the feed.<br/>",
 		description:" ",
 		date:""
 	};
 
 rsscontentdata.news[2] = {
-		link:"<?=url::base().'students'?>",
-		title:"Applicants, please click here to login into lakshya website and view your scholarship application status",
+		link:"<?=url::base().'scholarships'?>",
+		title:"< Scholarships > <br/><br/>Final list of scholars shortlisted 
+
+for funding have been uploaded. <br/> Click here to view them.",
 		description:" ",
 		date:""
 	};

89e4b3174b5508eae4bf8e6e2398ba1877ecb655
Sai Prasad <saiprasad.ch@gmail.com>
2011-03-05 21:04:17 +0530

    [code] Fixing the Scoller changes borken.

--- www/application/views/news/scroller.php
+++ www/application/views/news/scroller.php
@@ -1,8 +1,6 @@
 <?
 function slashit($what){ //Encode text for storing in JavaScript array
-$newstring=str_replace('&apos;', '\'', $what); //replace those half valid apostrophe 
-
-entities with actual apostrophes
+$newstring=str_replace('&apos;', '\'', $what); //replace those half valid apostrophe entities with actual apostrophes
 return rawurlencode($newstring);
 }
 ?>
@@ -10,27 +8,21 @@ rsscontentdata.news=new Array();
 
 rsscontentdata.news[0] = {
 		link:"<?=url::base().'library'?>",
-		title:"<   Library project   > <br/><br/>Encouraging students to 
-
-embrace the book reading habit. <br/> Donate a book today!",
+		title:"<   Library project   > <br/><br/>Encouraging students to embrace the book reading habit. <br/> Donate a book today!",
 		description:" ",
 		date:""
 	};
 
 rsscontentdata.news[1] = {
 		link:"<?=url::base().'innovation'?>",
-		title:"<   Innovation project   > <br/><br/>Live coverage of the 
-
-presentations streamed at Lakshya Live!! <br/>Click here to view the feed.<br/>",
+		title:"<   Innovation project   > <br/><br/>Live coverage of the presentations streamed at Lakshya Live!! <br/>Click here to view the feed.<br/>",
 		description:" ",
 		date:""
 	};
 
 rsscontentdata.news[2] = {
 		link:"<?=url::base().'scholarships'?>",
-		title:"< Scholarships > <br/><br/>Final list of scholars shortlisted 
-
-for funding have been uploaded. <br/> Click here to view them.",
+		title:"< Scholarships > <br/><br/>Final list of scholars shortlisted for funding have been uploaded. <br/> Click here to view them.",
 		description:" ",
 		date:""
 	};

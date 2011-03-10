6f9f4114437023fc150f13e1b7cf7e7be0b9d0ab
Anand Rajagopalan <anandr31@anandr31-laptop.(none)>
2011-03-10 21:06:26 +0530

    [code] added filters to donations page + CSS changes

--- www/application/views/accounts/allDonations.php
+++ www/application/views/accounts/allDonations.php
@@ -3,22 +3,55 @@
   @import url(/css/tableSort.css);
 -->
 </STYLE>
+    <script src="http://ajax.microsoft.com/ajax/jquery/jquery-1.3.2.min.js" type="text/javascript"></script>    		    
+    <script type="text/javascript" src="/scripts/tablefilter.js"></script>	
+    <script type="text/javascript"> 
+	//For table filter plugins - Quickfind and RowCount
+	$(document).ready(function() {
 
-<div align="right">
-Updated on: March 09, 2011
-</div>
+		var donTable = $('#donTable');
+            // Initialise Plugin
+            var options1 = {
+                additionalFilterTriggers: [$('#quickfind')],
+		filteredRows: function(filterStates)
+		{      															
+		donTable.removeClass('filtering');					
+		setRowCountOnDonTable();
+                }
+
+            };
+
+			function setRowCountOnDonTable() {
+				var rowcount = donTable.find('tbody tr:not(:hidden)').length;
+				$('#rowcount').text('(' + rowcount + ' rows displayed)');
+			}
+			
+			donTable.tableFilter(options1);
+			setRowCountOnDonTable();
+	});
+    </script>
+<!-- Table sorting: http ://www.frequency-decoder.com/-->
+<!-- Table filtering: http ://www.picnet.com.au/picnet_table_filter.html-->
 <br/>
 <center>
+<table width="100%">
+<tr>
+<td>Quick Find: <input type="text" id="quickfind"/></td>
+<td><div align="right">Updated on: March 09, 2011</div></td>
+</tr>
+</table>
+
+<span id='rowcount'></span>
 <!-- old header row colour: bgcolor="#D7D6E9" -->
 <!-- old body colour: bgcolor="#efefef" -->
 <table id="donTable" class="sort sortable-onload-5-reverse rowstyle-alt no-arrow" width="100%" border="0" cellspacing="1">
 <thead>
 	<tr class="sort">
-		<th class="sort fd-column-0 sortable-text"><a title="Sort on “Name”" href="#"><b>Name</b></th>
-		<th class="sort fd-column-1 sortable-numeric"><a title="Sort on “Batch”" href="#"><b>Batch</b></th>
-		<th class="sort fd-column-2 sortable-text"><a title="Sort on “Branch”" href="#"><b>Branch</b></th>
-		<th class="sort fd-column-3 sortable-numeric "><a title="Sort on “Amount”" href="#"><b>Amount</b></th>
-		<th class="sort fd-column-4 sortable-date-dmy"><a title="Sort on “Last Donated on”" href="#"><b>Last Donated on</b></th>
+		<th filter-type='text' class="sort fd-column-0 sortable-text"><a title="Sort on “Name”" href="#"><b>Name</b></th>
+		<th filter-type='ddl' class="sort fd-column-1 sortable-numeric" width="15%"><a title="Sort on “Batch”" href="#"><b>Batch</b></th>
+		<th filter-type='ddl' class="sort fd-column-2 sortable-text" width="15%"><a title="Sort on “Branch”" href="#"><b>Branch</b></th>
+		<th filter-type='text' class="sort fd-column-3 sortable-numeric "><a title="Sort on “Amount”" href="#"><b>Amount</b></th>
+		<th filter-type='text' class="sort fd-column-4 sortable-date-dmy"><a title="Sort on “Last Donated on”" href="#"><b>Last Donated on</b></th>
 	</tr>
 </thead>
 <tbody>

dcd76c54784559b0591f2fb41f29d62d58f559fc
Sai Prasad <saiprasad.ch@gmail.com>
2011-03-25 00:34:08 +0530

    [code] Adding some enum values for status & others

---
+++ www/application/helpers/enum.php
@@ -0,0 +1,27 @@
+<?php
+class enum {
+	
+	const NITW_RELATED = "yes";
+	const NITW_UNRELATED = "no";
+	
+	const BOOK_CURRICULUM = "cur";
+	const BOOK_NONCURRICULUM = "non";
+	
+	const PAYMENT_MODE_CASH = "cash";
+	const PAYMENT_MODE_CHEQUE = "cheque";
+	const PAYMENT_MODE_WEB = "web";
+	const PAYMENT_MODE_NEFT = "neft";
+	
+	const BOOK_STATUS_DONATED = "donated";
+	const BOOK_STATUS_PENDING = "pending";
+	const BOOK_STATUS_CANCELLED = "cancelled";
+	
+	const CLOTHES_STATUS_DONATED = "donated";
+	const CLOTHES_STATUS_PENDING = "pending";
+	const CLOTHES_STATUS_CANCELLED = "cancelled";
+	
+	const NEWSLETTER_SUB_ACTIVE = "active";
+	const NEWSLETTER_SUB_PENDING = "pending";
+	const NEWSLETTER_SUB_CANCELLED = "cancelled";
+	
+}
---
+++ www/application/helpers/status.php
@@ -0,0 +1,8 @@
+<?php
+class status {
+	
+	const DELETED = 0x1;
+	const DISABLED = 0x2;
+	const EMAIL_ACTIVED = 0x4;
+}
+?>

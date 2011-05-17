bf33dbd1012b143916d0f4d489e8be4602c92ca1
Anand Rajagopalan <anandr31@gmail.com>
2011-05-17 20:52:51 +0530

    changes made to innovation project page

--- www/application/views/home/innovation.php
+++ www/application/views/home/innovation.php
@@ -34,22 +34,38 @@
 		}
 	});
 </script>
-<p>Technology plays a key role in shaping a societies future.Where better to search for innovative solutions than engineering colleges.If the National Innovation Foundation can reap benefits of rural innovations, one can only wonder what potential lay in students.</p>
-<p>The Innovation Project is an incentive for students to pursue technology as a tool to overcome today's difficulties. </p>
-<p>India's jugaad philosophy urgently needs an upgrade and we're hoping it will come from a few of these bright young minds.</p>
-<p>We will be calling for abstracts and after a rigorous process, a few will be given the opportunity to transform their idea into and prototype and, if its a hit, into a product.</p>
+<b>Project Goal:</b> To fund ideas by NITW students to convert them into working models, prototypes or technology demonstrators.<br/>
+<p>The Innovation Project is an incentive for students to pursue technology as a tool to overcome today's difficulties.
+India's jugaad philosophy urgently needs an upgrade and we're hoping it will come from a few of these bright young minds.</p>
 <br/>
-<h2>Project proposal to be supported</h2>
-Selected after the final presentation rounds
+<b>Project proposal to be supported for 2011:</b>
 <ul>
-<li><span class="projectTitle">Hovomarine</span>
-	<ul>
-	<li>Joby Yeldo (M.Tech 1st Year - Manufacturing Engg.)</li>
-	</ul>
-</li>
+<li><span>Hovomarine by Joby Yeldo (M.Tech 1st Year - Manufacturing Engg.)</span></li>
 </ul>
-</blockquote>
+<br/>
+<b>Entries are closed for now. The competition will reopen in the second half of 2011. Dates for abstract submission will be posted here.</b>
+<h2>Competition</h2>
+<ol>
+  <li><a href="<?=url::base().'images/downloads/basic_info.pdf'?>" target="_blank">Basic Information</a></li>
+  <li><a href="<?=url::base().'images/downloads/TIP_Abstract_guidelines_V3.pdf'?>" target="_blank">Abstract Submission Guidelines</a></li>
+  <li><a href="<?=url::base().'images/downloads/TIP_FinalRd_guidelines_V3.pdf'?>" target="_blank">Final Round Guidelines.</a>   </li>
+</ol>
+<h2>Important Dates for the 2011 competition</h2>
+<ol>
+  <li>January 22nd 2011  - Entries Close</li>
+  <li>February 13th 2011 - Shortlist for Final Round.</li>
+  <li>March 5th,6th 2011 - Presentations by shortlisted Teams.</li>
+</ol>
+<h2>Important Points</h2>
+<ol>
 
+  <li><b>Mail all entries to info@TheLakshyaFoundation.org.</b></li>
+  <li>Subject line should be &quot;The Innovation Project&quot;</li>
+  <li>Entries on improving existing technologies will also be accepted.</li>
+  <li>More than one project may be funded.</li>
+  <li>For more information, shoot an email to info@TheLakshyaFoundation.org.</li>
+  <li>This competition is <b>NOT</b> open for final year B.Tech students. This is because we would like the entire team to be available to work on the winning idea while in college.</li>  
+</ol>
 <h2>The Innovation Project Poster.</h2>
 <p align="center">Click on the image to enlarge it.</p>
 <table width="660" border="0" align="center">
@@ -68,30 +84,7 @@ Selected after the final presentation rounds
 		title="Click to enlarge" /></a></td>
   </tr>
 </table>
-<br/>
-<h2>Competition</h2>
-<ol>
-  <li><a href="<?=url::base().'images/downloads/basic_info.pdf'?>" target="_blank">Basic Information</a></li>
-  <li><a href="<?=url::base().'images/downloads/TIP_Abstract_guidelines_V3.pdf'?>" target="_blank">Abstract Submission Guidelines</a></li>
-  <li><a href="<?=url::base().'images/downloads/TIP_FinalRd_guidelines_V3.pdf'?>" target="_blank">Final Round Guidelines.</a>   </li>
-</ol>
-<h2>Important Dates: Revised on January 26th </h2>
-<ol>
-  <li>January 22nd 2011  - Entries Close</li>
-  <li>February 13th 2011 - Shortlist for Final Round.</li>
-  <li>March 5th,6th 2011 - Presentations by shortlisted Teams.</li>
-</ol>
-<h2>Important Points</h2>
-<ol>
 
-  <li><b>Mail all entries to info@TheLakshyaFoundation.org.</b></li>
-  <li>Subject line should be &quot;The Innovation Project&quot;</li>
-  <li>Entries on improving existing technologies will also be accepted.</li>
-  <li>More than one project may be funded.</li>
-  <li>For more information, shoot an email to info@TheLakshyaFoundation.org.</li>
-  <li>This competition is <b>NOT</b> open for final year B.Tech students. This is because we would like the entire team to be available to work on the winning idea while in college.</li>  
-</ol>
-<p>&nbsp;</p>
 <center>
 <object width="500" height="350"><param name="movie" value="http://www.youtube.com/v/m_ho7xhgWV8&hl=en&fs=1&rel=0"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/m_ho7xhgWV8&hl=en&fs=1&rel=0" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="500" height="350"></embed></object>
 </center>

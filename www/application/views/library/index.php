72221a5960827b2c3d9483e7e12e9ff6591f3644
Nagendra <nkovid@gmail.com>
2011-06-18 08:33:50 +0530

    cleaned up library page

--- www/application/views/library/index.php
+++ www/application/views/library/index.php
@@ -31,27 +31,26 @@
 </script>
 <!-- code for library poster to zoom : END-->
 
-<p><b>Project Goal:</b> Providing curriculum/non-curriculum books to help
+<h5>Project Goal:</h5> <ul>Providing curriculum/non-curriculum books to help
 students widen their horizon on various subjects.
-<p>
-<p>Lakshya's Library Project is an initiative meant to provide
+</ul>
+<ul>Lakshya's Library Project is an initiative meant to provide
 curriculum/non-curriculum books covering a wide spectrum of subjects to
 help the student read, think and base opinions on sound judgement and
 knowledge. We look to provide books ranging from novels and biographies
-to non-fiction, books on history, and much more.</p>
-<p>No medium can influence the human psyche more than a book. Religions,
+to non-fiction, books on history, and much more.</ul>
+<ul>No medium can influence the human psyche more than a book. Religions,
 economies and even military doctrines have risen from works of authors
 who poured their thoughts in their writings for future generations to
-read and learn.</p>
+read and learn.</ul>
 
 <center><span style="font-size: 48px;">
 <button onclick="window.location.href='/library/donate'" style="height: 40px; width: 175px"><b>Donate a Book now</b></button></span></center>
 
-<br />
-<span style="color: green; font-size: 14px;"><b>Working Mechanism</b></span>
+<h5>Working Mechanism</h5>
 <center><a href="/library/donate"><img height=196 width=600 src="/images/library/Library_Workflow.png" /></a></center>
-<div id="list"><span style="color: green; font-size: 14px;">
-<b>Recent Donations</b></span><br />
+<div id="list">
+<h5>Recent Donations</h5>
 <style>
 .coTab {
 	border: 1px solid #D7D6E9;
@@ -64,7 +63,6 @@ read and learn.</p>
 	text-align: left;
 }
 </style>
-<br />
 <center>
 <table width="100%" border="0" class="coTab" cellspacing="1">
 	<tr>
@@ -99,12 +97,11 @@ read and learn.</p>
 </center>
 
 </div>
-<br/>
 <div style="text-align:right;font-size:+1.2em"><b><u><a href="/library/donations/">View all donations &raquo;</a></u></b></div>
 
 <div id="stats">
 <span style="color: green; font-size: 14px;">
-<b>Statistics</b></span><br />
+<h5>Statistics</h5></span>
 <table width="100%" border="0" cellspacing="2" cellpadding="5">
 <tr>
 <td width="70%">

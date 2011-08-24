40eb400a01698fb6eea05dbb9156ca2fd650474a
Nagendra <nkovid@gmail.com>
2011-08-24 23:49:36 +0530

    Added Foundations And Chapters and updated the donations page

--- www/application/views/home/scholarships.php
+++ www/application/views/home/scholarships.php
@@ -10,21 +10,43 @@ else
 }
 </script>
 <div id="overview">
-<h5>Project Goal:</h5><ul>To fund NIT Warangal students in need of financial assistance.</ul>
-<h5>Loan model:</h5>
-<ul>Raising funds is a challenge which gets tougher every year. To make this project sustainable, the Lakshya team has been working on a loan based model which
-was implemented for all scholar who applied in December 2010-2011.</ul>
+<p>Raising funds is a challenge which gets tougher every year. To make this project sustainable, the Lakshya team has switched to a loan based model. </p>
 <h5>Terms of the Loan:</h5>
 <ul>
 <li><b>Interest free loans without collateral</b>.</li>
 <li>The repayment period will be 5 years from graduation.</li>
 <li>To facilitate repayment tracking, each scholar will have to mandatorily pay a minimum of Rs.100 per month as soon as he/she graduates.</li>
 </ul>
-<h5>The advantages:</h5>
+<p>Many individuals, foundations and groups have come forward to generously support students chosen by Lakshya.</p>
+<h3>Foundations and Chapters</h3>
+<h5>Tom Zacharia Foundation</h5>
+<ul>The foundation is supporting Lakshya by funding one student every year.Tom Zacharia foundation was established on 17 Dec 2010 by Tom's family. 
+The key objective of the foundation is to keep the memory of Tom alive by supporting the values he held close to his heart - brilliance in your work,
+giving back to society, forging friendships across race and religion, and loving with a fire.</ul>
+<ol>The foundation aspires to achieve the following:
+<li>Support those who wish to pursue a professional career in science or engineering but do not have the means to do so.</li>
+<li>Provide financial support and or scholarships to deserving children.</li>
+<li>Provide support to other foundations / institutions that propagate and/or stand for above described causes.</li>
+</ol>
+<ul>The foundation is primarily funded by the family. Secondary funding comes from royalties of the book "Live Like Tom" being published by Bency and Zac Thomas.</ul>
+<h5>NITW Americas Chapter</h5>
+<ul>The NITW Americas Chapter is a registered organization set up by the NITW alumni in the United States and has agreed to provide tuition and hostel fees for 
+three students every year.</ul>
+<h5>Trahi Foundation</h5>
+<ul>Trahi Foundation will be funding two students every year.The Trahi Foundation is an organization formed by the 1993 batch NITW alumni 
+and is based in Bangalore.</ul>
+<h5>NITW Middle East Chapter</h5>
+<ul>The Middle East chapter is funding one student every year. The NITW Middle East Chapter has been in constant touch with Lakshya and, in 2011, 
+came forward to fund one student every year.</ul>
+<h5>BVS Murty 1979-83 NITW Alumni Scholarship & K Srikant Murty 1979-83 NITW Alumni Scholarship</h5>
+<ul>Each of the two scholarships will be funding one student every year. The 1979-83 batch collected
+funds for two scholarships every year, and has named them after two of their deceased batchmates.</ul>
+<h5>Individual contributors</h5>
+<ul>Each of the below mentioned donors are currently sponsoring one Lakshya scholar
+<li><b>Mr. Baiju Vyas </b> (B.Tech NITW, Class of 1977 B.Tech)</li>
+<li><b>Mr. Prabhakar Puvvada</b> (B.Tech NITW, Class of 1980)</li>
+<li><b>Mr. Narayan</b> (B.Tech NITW, Class of 1983)</li>
 <ul>
-<li>Makes this project sustainable and will reduce the burden on fund-raising.</li>
-<li>Coupled with donations, it will help accomodate more scholars every year.</li>
-</ul>
 </div>
 <h5>The Lakshya Scholars:</h5>
         <ul class="mytabs" id="tabs">
